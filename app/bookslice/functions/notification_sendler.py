7be3b85fd9eedf3f5c139f824ebbf38f73179303
Alex <Sashayerty@ya.ru>
2024-11-30 23:22:14 +0300

    Добавил создание сессии бд

--- app/bookslice/functions/notification_sendler.py
+++ app/bookslice/functions/notification_sendler.py
@@ -1,6 +1,19 @@
+from sqlalchemy.orm import Session
+
+from app.models.db_session import create_session, global_init
+from app.models.notifications import Notifications
+
+
+def session_create() -> Session:
+    global_init("app/app.db")
+    db_session = create_session()
+    return db_session
+
+
 def send_to_user_friend_request_notification(
     user_id: int,
     friend_id: int,
-    db_session,
+    data: str | int,
+    db_session: Session = session_create(),
 ) -> bool:
-    pass
+    notifications = db_session.query(Notifications)  # noqa

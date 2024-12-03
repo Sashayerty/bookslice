1b9c23f8522533cc4afb13bb656ebdfaf602f611
Alex <Sashayerty@ya.ru>
2024-12-03 20:37:57 +0300

    Сделал для проверок

--- app/bookslice/functions/AI.py
+++ app/bookslice/functions/AI.py
@@ -1,6 +1,6 @@
-from mistralai import Mistral
 from dotenv import dotenv_values
 from markdown import markdown
+from mistralai import Mistral
 
 api_key = dotenv_values("./.env")["MISTRAL_API_KEY"]
 
@@ -9,22 +9,22 @@ class AI:
     def __init__(self) -> None:
         self.client = Mistral(api_key=api_key)
         self.msgs = []
-        self.promt = {'messages': self.msgs}
+        self.promt = {"messages": self.msgs}
 
     def message(self, mess):
         self.msgs.append({"role": "user", "text": mess})
         self.promt["messages"] = self.msgs
         self.chat_response = self.client.chat.complete(
-            model = "mistral-large-latest",
-            messages = [
+            model="mistral-large-latest",
+            messages=[
                 {
                     "role": "user",
                     "content": f"""Ты - ИИ помощник для сайта-соцсети, посвещённой чтению. Ты должен поддерживать диалог, нейтрально относясь к острым темам, и выдавать сухую информацию о них:
                     религия, политика, национальность. Ответы должны быть ТОЛЬКО на русском. История сообщений с юзером:{self.msgs}""",
                 },
-            ]
+            ],
         )
-        response = markdown(self.chat_response.choices[0].message.content) 
+        response = markdown(self.chat_response.choices[0].message.content)
         self.promt["messages"].append(
             {
                 "role": "assistant",
@@ -32,7 +32,6 @@ class AI:
             }
         )
         return response
-    
 
     def get_messages(self):
         return self.promt["messages"]

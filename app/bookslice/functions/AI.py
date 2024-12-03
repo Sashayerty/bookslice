919024a261512fb6d892271c04fa15fdff6ee5ec
John <ivanngorin@yandex.ru>
2024-12-03 20:26:23 +0300

    Поменял AI.py + поменял вывод от ИИ

--- app/bookslice/functions/AI.py
+++ app/bookslice/functions/AI.py
@@ -1,47 +1,41 @@
-import requests
+from mistralai import Mistral
 from dotenv import dotenv_values
+from markdown import markdown
 
-key = dotenv_values("./.env")["YANDEX_KEY"]
+api_key = dotenv_values("./.env")["MISTRAL_API_KEY"]
 
 
 class AI:
     def __init__(self) -> None:
-        self.key = key
+        self.client = Mistral(api_key=api_key)
         self.msgs = []
-        self.prompt = {
-            "modelUri": "gpt://b1gpchf8l5umrbhroffm/yandexgpt-lite",
-            "completionOptions": {
-                "stream": False,
-                "temperature": 0.6,
-                "maxTokens": "5000",
-            },
-            "messages": self.msgs,
-        }
-        self.url = (
-            "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-        )
-        self.headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Api-Key {self.key}",
-        }
+        self.promt = {'messages': self.msgs}
 
     def message(self, mess):
         self.msgs.append({"role": "user", "text": mess})
-        self.prompt["messages"] = self.msgs
-        response = requests.post(
-            self.url, headers=self.headers, json=self.prompt
+        self.promt["messages"] = self.msgs
+        self.chat_response = self.client.chat.complete(
+            model = "mistral-large-latest",
+            messages = [
+                {
+                    "role": "user",
+                    "content": f"""Ты - ИИ помощник для сайта-соцсети, посвещённой чтению. Ты должен поддерживать диалог, нейтрально относясь к острым темам, и выдавать сухую информацию о них:
+                    религия, политика, национальность. Ответы должны быть ТОЛЬКО на русском. История сообщений с юзером:{self.msgs}""",
+                },
+            ]
         )
-        result = response.json()
-        self.prompt["messages"].append(
+        response = markdown(self.chat_response.choices[0].message.content) 
+        self.promt["messages"].append(
             {
                 "role": "assistant",
-                "text": result["result"]["alternatives"][0]["message"]["text"],
+                "text": response,
             }
         )
-        return result
+        return response
+    
 
     def get_messages(self):
-        return self.prompt["messages"]
+        return self.promt["messages"]
 
     def set_messages(self, messages):
         self.msgs = messages
